72432f529b7f06e21f39ca83fc4f78ddab21b3a1
Filming <79931565+filming@users.noreply.github.com>
2024-08-27 19:51:14 -0400

    feat: Add a logger for XIFY objects

--- src/xify/__init__.py
+++ src/xify/__init__.py
@@ -1,3 +1,29 @@
+import logging
+import os
+
+
 class Xify:
+    """A class that provides methods for interacting with the X API."""
+
     def __init__(self) -> None:
-        pass
+        # Make sure storage location exists before creating logging obj
+        LOG_DIR_PATH = os.path.join("storage", "logs")
+        LOG_PATH = os.path.join(LOG_DIR_PATH, "xify.log")
+        os.makedirs(LOG_DIR_PATH, exist_ok=True)
+
+        # Setup a logger for this xify object
+        logger = logging.getLogger(__name__)  # Get a logger with the class name
+        logger.setLevel(logging.INFO)
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+
+        handler = logging.FileHandler(LOG_PATH, "w")
+        handler.setFormatter(formatter)
+
+        logger.addHandler(handler)
+
+        self.logger = logger
+        self.logger.info("Xify initialized successfully!")
