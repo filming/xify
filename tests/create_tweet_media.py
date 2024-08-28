c3b978f219fb5ead7b6786cf10a32f916ddf5f6d
Filming <79931565+filming@users.noreply.github.com>
2024-08-28 19:12:04 -0400

    test: Retrieve a media ID for file

---
+++ tests/create_tweet_media.py
@@ -0,0 +1,31 @@
+from src.xify import Xify
+
+
+def main():
+    xify = Xify()
+    xify.create_xas()
+
+    message_content = "Good evening!"
+
+    # Get media ids for locally stored media
+    filepaths = [
+        "./storage/media/candy.png",
+        # "./storage/media/charlie.png",
+        # "./storage/media/flower.jpg",
+        # "./storage/media/snoopy.jpg",
+        # "./storage/media/sunset.gif",
+    ]
+    media_ids = []
+
+    for filepath in filepaths:
+        media_id = xify.create_media_id(filepath)
+        media_ids.append(media_id)
+
+    print(media_ids)
+    # tweet_id = xify.create_tweet(message_content, media_ids)
+
+    # print(tweet_id)
+
+
+if __name__ == "__main__":
+    main()
